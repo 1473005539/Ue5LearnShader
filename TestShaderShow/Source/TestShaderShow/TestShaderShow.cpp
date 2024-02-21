9976cea2c9f9b300481f83e38dec56375fa8ae18
XingHua Hu <1473005539@qq.com>
2024-02-21 10:55:38 +0800

    Fire Commit Data

---
+++ TestShaderShow/Source/TestShaderShow/TestShaderShow.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestShaderShow.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestShaderShow, "TestShaderShow" );
---
+++ TestShaderShow/Source/TestShaderShow/TestShaderShowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestShaderShowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTSHADERSHOW_API ATestShaderShowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
